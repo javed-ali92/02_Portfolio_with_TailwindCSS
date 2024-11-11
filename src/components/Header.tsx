ad91b12fb78d10cab511a3a3204fdc1c054e8aac
“[javed-ali92] <[javed.ali.1121974@gmail.com]>
2024-11-11 12:10:19 +0500

    first commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,31 @@
+import Link from 'next/link';
+
 export default function Header() {
-    return (
-      <div className="flex justify-between items-center bg-[#464545] text-white p-5">
-        <div className="text-3xl font-serif">Portfolio</div>
-        <ul className="flex space-x-5">
-          <li>
-            <a href="/" className="font-serif hover:text-[#292928] transition duration-300">
+  return (
+    <div className="flex justify-between items-center bg-[#464545] text-white p-5">
+      <div className="text-3xl font-serif">Portfolio</div>
+      <ul className="flex space-x-5">
+        <li>
+          <Link href="/" className="font-serif hover:text-[#292928] transition duration-300">
             Home
-            </a>
-          </li>
-          <li>
-            <a href="/about" className="font-serif hover:text-[#292928] transition duration-300">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="/project" className="font-serif hover:text-[#292928] transition duration-300">
-              Projects
-            </a>
-          </li>
-          <li>
-            <a href="/contact" className="font-serif hover:text-[#292928] transition duration-300">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+          </Link>
+        </li>
+        <li>
+          <Link href="/about" className="font-serif hover:text-[#292928] transition duration-300">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link href="/project" className="font-serif hover:text-[#292928] transition duration-300">
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact" className="font-serif hover:text-[#292928] transition duration-300">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
